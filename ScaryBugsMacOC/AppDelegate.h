d132e19ed9025f68833cfa784d2fac1b8cb0fcd2
Leo Yu <yuleo@netscape.net>
2015-09-26 15:01:14 -0700

    Initial Commit

---
+++ ScaryBugsMacOC/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScaryBugsMacOC
+//
+//  Created by Leo Yu on 9/26/15.
+//  Copyright © 2015 Leo Yu. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ScaryBugsMacOC/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ScaryBugsMacOC
+//
+//  Created by Leo Yu on 9/26/15.
+//  Copyright © 2015 Leo Yu. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
